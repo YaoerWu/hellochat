2c23968402959aa4929760a66bcb35ba2585d248
HoneyYolk <honey_yolk@outlook.com>
2022-02-06 12:17:12 +0800

    basic version

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{self, BufRead, BufReader, Write};
+use std::net::TcpStream;
+use std::thread;
+
+const REMOTE_HOST: &str = "127.0.0.1:8888";
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = TcpStream::connect(REMOTE_HOST).unwrap();
+    let mut reader = BufReader::new(stream.try_clone().unwrap());
+
+    thread::spawn(move || loop {
+        let mut buf = vec![];
+        match reader.read_until(b'}', &mut buf) {
+            Err(_) | Ok(0) => {
+                println!("Connection reset by remote");
+                break;
+            }
+            Ok(_) => {
+                let mut msg = String::from_utf8(buf).expect("Convert failed");
+                msg.pop();
+                println!("{}", msg);
+            }
+        }
+    });
+    loop {
+        let mut msg = String::new();
+        io::stdin()
+            .read_line(&mut msg)
+            .expect("Failed to read line");
+        msg = msg.trim().to_string();
+        msg.push('}');
+        stream.write_all(msg.as_bytes()).unwrap();
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,104 @@
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc;
+use std::thread;
+
+const LOCAL_HOST: &str = "127.0.0.1:8888";
+
+enum Message {
+    Connection(usize, TcpStream),
+    ConnectionReset(usize),
+    Client(usize, String),
+}
+
 fn main() {
-    println!("Hello, world!");
+    //Initialzation
+    let listener = TcpListener::bind(LOCAL_HOST).expect("Fail to bind socket");
+    println!("Listener up and running");
+    let (tx, rx) = mpsc::channel();
+
+    //Connection controller
+    thread::spawn(move || {
+        let mut client_id = 0;
+        let mut get_client_id = || -> usize {
+            client_id += 1;
+            client_id
+        };
+        for stream in listener.incoming() {
+            match stream {
+                Err(e) => println!("Connection failed: {}", e),
+                Ok(stream) => {
+                    let client_id = get_client_id();
+                    tx.send(Message::Connection(
+                        client_id,
+                        stream.try_clone().expect("Can't clone stream"),
+                    ))
+                    .expect("Failed to send message");
+                    let client_tx = tx.clone();
+
+                    //TCP reader
+                    thread::spawn(move || {
+                        let mut reader = BufReader::new(stream);
+                        loop {
+                            let mut buf = vec![];
+                            match reader.read_until(b'}', &mut buf) {
+                                Err(_) | Ok(0) => {
+                                    client_tx
+                                        .send(Message::ConnectionReset(client_id))
+                                        .expect("Unreachable message");
+                                    break;
+                                }
+                                Ok(_) => client_tx
+                                    .send(Message::Client(
+                                        client_id,
+                                        String::from_utf8(buf).expect("Convert failed"),
+                                    ))
+                                    .expect("Unreachable message"),
+                            };
+                        }
+                    });
+                }
+            }
+        }
+    });
+
+    let mut client_map = HashMap::new();
+
+    //Initialized
+    loop {
+        handle_message(
+            rx.recv().expect("Failed to receive message"),
+            &mut client_map,
+        );
+    }
+}
+
+fn handle_message(msg: Message, client_map: &mut HashMap<usize, TcpStream>) {
+    match msg {
+        Message::Connection(client_id, stream) => {
+            client_map.insert(client_id, stream);
+            println!("client {} connected", client_id);
+        }
+        Message::ConnectionReset(client_id) => {
+            client_map.remove(&client_id);
+            println!("client {} disconnected", client_id);
+        }
+        Message::Client(id, mut msg) => {
+            msg.pop();
+            println!("Message from client {} : {}", id, msg);
+            for client in client_map.iter() {
+                let (client_id, mut client_stream) = client;
+                if *client_id == id {
+                    continue;
+                } else {
+                    let msg = format!("client {} : ", id) + &msg;
+                    client_stream
+                        .write_all(msg.as_bytes())
+                        .expect("Failed to write message");
+                    println!("Message sent to client : {}", client_id);
+                }
+            }
+        }
+    }
 }
