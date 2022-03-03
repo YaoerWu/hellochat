2d7cca66df7d1a4e4bd3d1841e408cd250304157
HoneyYolk <honey_yolk@outlook.com>
2022-03-03 18:59:56 +0800

    change client to current thread

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,36 +1,39 @@
 use std::io;
 use tokio::net::TcpStream;
+use tokio::runtime::Builder;
 
-const REMOTE_HOST: &str = "127.0.0.1:8888";
+fn main() {
+    const REMOTE_HOST: &str = "SomeHost";
+    let rt = Builder::new_current_thread().build().unwrap();
+    rt.block_on(async {
+        let stream = TcpStream::connect(REMOTE_HOST).await.unwrap();
 
-#[tokio::main]
-async fn main() {
-    let stream = TcpStream::connect(REMOTE_HOST).await.unwrap();
-    let (reader, writer) = stream.into_split();
+        let (reader, writer) = stream.into_split();
 
-    tokio::spawn(async move {
-        loop {
-            let mut buf = vec![];
-            match reader.try_read(&mut buf) {
-                Err(_) | Ok(0) => {
-                    println!("Connection reset by remote");
-                    break;
-                }
-                Ok(_) => {
-                    let msg = buf;
-                    println!("{}", String::from_utf8(msg).unwrap().trim());
+        tokio::spawn(async move {
+            loop {
+                let mut buf = vec![];
+                match reader.try_read(&mut buf) {
+                    Err(_) | Ok(0) => {
+                        println!("Connection reset by remote");
+                        break;
+                    }
+                    Ok(_) => {
+                        let msg = buf;
+                        println!("{}", String::from_utf8(msg).unwrap().trim());
+                    }
                 }
             }
+        });
+        loop {
+            let mut msg = String::new();
+            io::stdin()
+                .read_line(&mut msg)
+                .expect("Failed to read line");
+            msg = msg.trim().to_string();
+            let mut buf = Vec::from(msg);
+            buf.push(0xa);
+            writer.try_write(&buf).unwrap();
         }
     });
-    loop {
-        let mut msg = String::new();
-        io::stdin()
-            .read_line(&mut msg)
-            .expect("Failed to read line");
-        msg = msg.trim().to_string();
-        let mut buf = Vec::from(msg);
-        buf.push(0xa);
-        writer.try_write(&buf).unwrap();
-    }
 }
